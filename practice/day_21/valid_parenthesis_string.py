0a4103173233ad2eb5bf349ad1e28523f85b6b1d
Anuranjan Jain <weebxaj@gmail.com>
2025-05-20 17:08:52 +0530

    Update valid_parenthesis_string.py

--- practice/day_21/valid_parenthesis_string.py
+++ practice/day_21/valid_parenthesis_string.py
@@ -5,3 +5,24 @@
 # Date: 2025-05-20
 # Status: Solved
 
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+        min_open, max_open = 0, 0 
+
+        for ch in s:
+            if ch == '(':
+                min_open += 1
+                max_open += 1
+            elif ch == ')':
+                min_open -= 1
+                max_open -= 1
+            else: 
+                min_open -= 1  
+                max_open += 1  
+            
+            if min_open < 0:  
+                min_open = 0
+            if max_open < 0: 
+                return False
+
+        return min_open == 0 
